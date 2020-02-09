71dd6b61716a0b6287d3b4374833eeedeae05a6d
Konstantin Perikov <pericov.ko@gmail.com>
2020-02-09 17:11:34 +0000

    add cpus detection, concurrent processing

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ cargo build
 ./target/debug/hello mandel.png 300 1600x1200 -1.20,0.35 -1.0,0.20
 ```
 
-<img src="/docs/mandelbrot/mandel.png?raw=true" width=60% height=60%/>
+<img src="/docs/mandelbrot/mandel.png?raw=true" width=100% height=100%/>
 
+2. 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate crossbeam;
 extern crate image;
 extern crate num;
 
@@ -133,7 +134,31 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right, limit);
+    let cpus = num_cpus::get();
+    println!("Number of CPUs detected: {}", cpus);
+    let threads = cpus * 4;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> =
+            pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left =
+                    pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height),
+                                   upper_left, lower_right);
+
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right, limit);
+                });
+            }
+        });
+    }
 
     write_image(&args[1], &pixels, bounds)
         .expect("error writing PNG file");
