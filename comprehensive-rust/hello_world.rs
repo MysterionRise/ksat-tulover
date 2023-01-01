7ea7f3721f039466aa3093810065367ad125e417
Konstantin Perikov <pericov.ko@gmail.com>
2023-01-01 21:36:20 +0000

    add pre-morning exercises

---
+++ comprehensive-rust/hello_world.rs
@@ -0,0 +1,37 @@
+fn main() {
+    println!("Hello 🌍!");
+
+    let mut x: i32 = 10;
+    print!("{x}");
+    while x != 1 {
+        if x % 2 == 0 {
+            x = x / 2;
+        } else {
+            x = 3 * x + 1;
+        }
+        print!(" -> {x}");
+    }
+    println!();
+
+    let mut a: [i8; 10] = [42; 10];
+    a[5] = 0;
+    println!("a: {:?}", a);
+
+    let t: (i8, bool) = (7, true);
+    println!("1st index: {}", t.0);
+    println!("2nd index: {}", t.1);
+
+    let a: [i32; 6] = [10, 20, 30, 40, 50, 60];
+    println!("a: {a:?}");
+
+    let s: &[i32] = &a[2..4];
+    println!("s: {s:?}");
+
+    let s1: &str = "Hello";
+    println!("s1: {s1}");
+
+    let mut s2: String = String::from("Hello ");
+    println!("s2: {s2}");
+    s2.push_str(s1);
+    println!("s2: {s2}");
+}
